33c09037a591db333e545c236b36d40b615e9bf7
andrew dillon <majik895@gmail.com>
2019-08-07 21:14:02 -0400

    added another .get

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,6 +2,20 @@ const express = 'express';
 const userdb = require('./userDb')
 const router = express.Router();
 
+function validateUserId(req, res, next) {
+    const { id } = req.params
+    userdb.getById(id)
+        .then(user => {
+            req.user = user
+            next()
+        })
+        .catch(error => {
+            res.status(400).json({ message: 'You put in the wrong ID dog' })
+        })
+}
+
+
+
 router.post('/', (req, res) => {
 
 });
@@ -20,8 +34,8 @@ router.get('/', (req, res) => {
         })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    res.status(200).json(req.user)
 });
 
 router.get('/:id/posts', (req, res) => {
