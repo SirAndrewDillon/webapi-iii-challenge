7550946c740d7b0603d3ec48a1be71e79760b40e
andrew dillon <majik895@gmail.com>
2019-09-04 14:34:41 -0400

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const server = require('./server.js')
+
+//Start server listening
+const port = process.env.PORT || 3333
+server.listen(port, () => {
+	console.log(`\n*** Server Running on http://localhost:${port} ***\n`)
+})
--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = 'express';
+const express = 'express'
+const userRouter = require('./users/userRouter')
+const postRouter = require('./posts/postRouter')
 
-const server = express();
+const server = express()
+server.use(express.json())
+server.use(logger)
+server.use('/api/user', userRouter)
+server.use('/api/posts', postRouter)
+
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h1>Let's hook up some some middleware!</h1>`)
 });
 
+
 //custom middleware
-
 function logger(req, res, next) {
-
+    let timestamp = new Date()toUTCString()
+    console.log(`Method: {req.method} | URL: {req.url} | Time: {timestamp}`)
 };
 
 module.exports = server;
+
+
+
+
+
+
+
+
